52c219fc2c8b8d774f94ba1f694147de6d1ce4b0
Imtiaz Ahmed Chowdhury <imtiazemon625@gmail.com>
2022-09-18 18:09:51 +0600

    Fronted

---
+++ src/components/Content.vue
@@ -0,0 +1,9 @@
+<template>
+     <div class="text-cyan-700 font-bold px-4 py-3">Content</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="relative flex min-h-screen">
+    <!-- Side Bar -->
+    <div class="bg-cyan-600 text-cyan-100 w-64">
+      <div class="flex justify-center items-center mt-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="{1.5}"
+          stroke="currentColor"
+          class="w-10 h-10"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z"
+          />
+        </svg>
+        <span class="text-4xl font-extrabold">Dev Hunt</span>
+      </div>
+      <nav class="flex justify-center py-3 px-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6 bg-slate-700 rounded-lg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+          />
+        </svg>
+         <span class="text-white px-2 font-medium">Home</span>
+      </nav>
+    </div>
+    <div class="flex-1">
+      <!-- Header -->
+      <Header></Header>
+      <!-- Content -->
+      <Content></Content>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+import Content from "./Content.vue";
+
+export default {
+  components: {
+    Header,
+    Content,
+  },
+};
+</script>
